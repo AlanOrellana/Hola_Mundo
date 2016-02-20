1261703350df7513ea63f9fd187955599187b793
AlanOrellana <alsaorellana@gmail.com>
2016-02-19 20:09:35 -0600

    Hola Mundo

---
+++ src/ejemplo1.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : ejemplo1.cpp
+// Author      : Jose Castro
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void saludar(char* nombre){
+	cout << "Hola" << endl; // prints !!!Hello World!!!
+}
+int main() {
+	saludar("Jose");
+	return 0;
+}
